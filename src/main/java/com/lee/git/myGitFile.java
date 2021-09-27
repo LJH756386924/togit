4ee4b5fa4351c81e3bb582c0552bda7df1e2ed77
jiahao.li <756386924@qq.com>
2021-09-27 11:19:24 +0800

    第二次使用Idea配置git并提交代码

--- src/main/java/com/lee/git/myGitFile.java
+++ src/main/java/com/lee/git/myGitFile.java
@@ -3,5 +3,10 @@ package com.lee.git;
 public class myGitFile {
    public static void main(String[] args){
        System.out.println("Hello Lee");
+       Integer git = 1;
+       Integer hub = 1;
+       if(git.equals(hub)&&(git+hub)>0){
+           System.out.println("github");
+       }
    }
 }
